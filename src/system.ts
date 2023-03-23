7df3397b262733bf95746ae2f7accd35175e5ac7
barry green <61643072+xuhairui1@users.noreply.github.com>
2023-03-23 18:54:18 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -7,7 +7,7 @@ export const setting = {
   systemRule: ""
 }
 
-export const message = `基于OpenAI API(gpt-3.5-turbo)，请勿将网页传播至公众平台
+export const message = `基于OpenAI API(gpt-3.5-turbo)，请勿将网页传播至公众平台，点左下角设置按钮-填写网站密码-即可使用
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
 
 export type Setting = typeof setting
